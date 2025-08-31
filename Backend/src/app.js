72d0574060c38ad1803768378b5da004573c9507
unknown <sensaurav25@gmail.com>
2025-08-31 22:27:23 +0530

    Deployment build

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const SongRoutes = require('./routes/song.routes.js');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 app.use(express.json());    
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../public')))
 app.use(cors());
 
 app.use('/', SongRoutes);
 
+app.get("*name", (req, res) =>{
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 module.exports = app;
